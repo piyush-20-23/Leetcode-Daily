c2d2265fa7309e4764f65bc818059f73d9779798
Piyush Patle <166403298+piyush-20-23@users.noreply.github.com>
2026-07-03 21:55:23 +0530

    Time: 755 ms (23.90%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,7 @@
+select 
+    s.user_id,
+    round(count(case when c.action = 'confirmed' then 1 end) / count(*), 2) as confirmation_rate
+from Signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
