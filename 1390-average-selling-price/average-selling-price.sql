2b47610312c9d967ed40f9616f6e626d32d7851e
Piyush Patle <166403298+piyush-20-23@users.noreply.github.com>
2026-07-04 02:36:44 +0530

    Time: 728 ms (99.60%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,21 +1,9 @@
 # Write your MySQL query statement below
-with cte as(
-    select 
-        p.*
-        ,u.purchase_date
-        ,u.units
-    from prices p
-    left join unitsSold u
-    on p.product_id = u.product_id
-        and u.purchase_date between p.start_date and p.end_date
-)
-
 select 
-    product_id
-    ,case when round(sum(price * units) / sum(units), 2) is null 
-        then 0 
-        else round(sum(price * units) / sum(units), 2 )
-        end as average_price
-from cte
-group by product_id
-
+    p.product_id
+    ,coalesce(round(sum(p.price * u.units) / sum(u.units), 2) , 0) as average_price
+from prices p
+left join unitsSold u
+on p.product_id = u.product_id
+    and u.purchase_date between p.start_date and p.end_date
+group by p.product_id
