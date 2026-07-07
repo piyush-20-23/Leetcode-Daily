94391a9e53c6a0c5f8b24c99f5e33f5ea3605253
Piyush Patle <166403298+piyush-20-23@users.noreply.github.com>
2026-07-07 22:14:52 +0530

    Time: 57 ms (96.58%), Space: 167.3 MB (5.04%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        // initialized a number line and placed the time required by each car to reach target
+        double[] noLine = new double[target];
+
+        for(int i = 0; i < n; i ++){
+            noLine[position[i]] = (double)(target - position[i] ) / speed[i];
+        }
+
+        double[] time = new double[n];
+        int pos = 0;
+        for(double ele : noLine){
+            if(ele != 0){
+                time[pos] = ele;
+                pos ++;
+            }
+        }
+
+
+        //System.out.println(Arrays.toString(time));
+
+        // implement a stack 
+
+        // if time required by the cars ahead is more => it will form a car fleet
+        int carFleet = 0;
+        Deque<Double> stack = new ArrayDeque();
+        stack.push(time[n - 1]);
+
+        for(int i = n - 2; i >= 0; i --){
+            if(stack.peek() > time[i]){
+                time[i] = stack.peek();
+            }
+            else {
+                stack.push(time[i]);
+            }
+        }
+
+        Set<Double> hs = new HashSet<>();
+
+        for(double ele : time){
+            hs.add(ele);
+        }
+
+        return hs.size();
+    }
+}
