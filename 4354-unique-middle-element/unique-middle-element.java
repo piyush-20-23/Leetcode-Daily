dde3f17465d1c7bdca43b6a2bc637ae33a0e104b
Piyush Patle <166403298+piyush-20-23@users.noreply.github.com>
2026-07-08 11:22:23 +0530

    Time: 1 ms (94.77%) | Memory: 46.5 MB (32.71%) - LeetSync

---
+++ 4354-unique-middle-element/unique-middle-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+
+        for(int i = 0; i < nums.length; i ++){
+            if(i == nums.length / 2)
+                continue;
+            if(nums[i] == nums[nums.length / 2]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
