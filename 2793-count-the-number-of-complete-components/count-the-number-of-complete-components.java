a9d21bf568a6e51aaa67dbc748b41a9e9693a041
Piyush Patle <166403298+piyush-20-23@users.noreply.github.com>
2026-07-12 23:02:48 +0530

    Time: 11 ms (59.27%) | Memory: 47.2 MB (97.17%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,40 +1,63 @@
 class Solution {
-    static int V, D;
+
+    boolean[] visited;
+    List<List<Integer>> adj;
+    int componentCount; 
+    int n;
+    int edg;
+    int ver;
 
     public int countCompleteComponents(int n, int[][] edges) {
-        List<Integer>[] A = new ArrayList[n];
-        Arrays.setAll(A, _ -> new ArrayList<>());
+        this.n = n;
+        // create adjacency List
+        adj = new ArrayList<>();
 
-        for (int[] e : edges) {
-            A[e[0]].add(e[1]);
-            A[e[1]].add(e[0]);
+        for(int i = 0; i < n; i ++){
+            adj.add( new ArrayList<>());
         }
 
-        boolean[] vis = new boolean[n];
-        int res = 0;
+        for(int i = 0; i < edges.length; i ++){
+            int u = edges[i][0];
+            int v = edges[i][1];
 
-        for (int i = 0; i < n; i++) {
-            boolean state = vis[i];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
 
-            if (!state) {
-                V = 0; D = 0;
+        // iterate through every node if it not visited by dfs
+        componentCount = 0;
+        visited = new boolean[n];
 
-                dfs(i, A, vis);
+        ver = 0;
+        edg = 0;
+        
+        for(int i = 0; i < n; i ++){
+            ver = 0;
+            edg = 0;
+            if(!visited[i]){
+                dfs(i);
+            }
 
-                if (D == V * (V - 1)) res++;
+            if((ver * (ver -1)) / 2 == edg/2 && ver != 0){
+                componentCount ++;
             }
         }
 
-        return res;
+
+        return componentCount;
     }
 
-    private void dfs(int x, List<Integer>[] A, boolean[] vis) {
-        V++;
-        D += A[x].size();
-        vis[x] = true;
-
-        for (int state : A[x])
-            if (!vis[state])
-                dfs(state, A, vis);
+    public void dfs(int node){
+        
+        visited[node] = true;
+        ver ++;
+        edg += adj.get(node).size();
+        
+        
+        for(int neighbour : adj.get(node)){
+            if(!visited[neighbour]){
+                dfs(neighbour);
+            }
+        }
     }
 }
