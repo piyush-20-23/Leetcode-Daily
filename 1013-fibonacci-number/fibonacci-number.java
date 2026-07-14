6af244762dc32cfaf80882e7aa54e4ae388f3c8f
Piyush Patle <166403298+piyush-20-23@users.noreply.github.com>
2026-07-15 00:47:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (15.99%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,20 +1,16 @@
 class Solution {
-    int[] memo;
-
-    public int helper(int n){
-        if(n == 0 || n == 1) 
-            return n;
-
-        if(memo[n] != -1){
-            return memo[n];
-        }        
-
-        memo[n] = helper(n - 1) + helper(n - 2);
-        return memo[n];
-    }
+    // bottom up sol with O(n) space
     public int fib(int n) {
-        memo = new int[n + 1];
-        Arrays.fill(memo, -1);
-        return helper(n);
+        if(n <= 1) 
+            return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i < n + 1; i ++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 }
